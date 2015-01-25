4b1ecc560f147c53d8798e11dc2744045d34e34c
H.B Pickering-Dunn <brad@pickering-dunn.com>
2015-01-25 10:40:07 +0200

    Basic project structure

---
+++ WiggleTechTest/Wiggle.Domain.Models/Products/ProductBasket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiggle.Domain.Models.Products
+{
+    public class ProductBasket : BaseDto
+    {
+        public IEnumerable<ProductDto> Products { get; set; }
+
+        public double Total { get; }
+    }
+}
