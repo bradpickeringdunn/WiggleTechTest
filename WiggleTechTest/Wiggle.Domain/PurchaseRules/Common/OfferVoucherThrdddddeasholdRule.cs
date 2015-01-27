afd884213d6c3808c6869ec2384d3c418107ef63
H.B Pickering-Dunn <brad@pickering-dunn.com>
2015-01-27 06:10:43 +0200

    Removed the backbone project and added the dll as a reference instead

---
+++ WiggleTechTest/Wiggle.Domain/PurchaseRules/Common/OfferVoucherThrdddddeasholdRule.cs
@@ -0,0 +1,77 @@
+﻿using Backbone.ErrorHandling;
+using Backbone.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wiggle.Domain.Models.Products;
+using Wiggle.Domain.Models.ShoppingBasket;
+
+namespace Wiggle.Domain.PurchaseRules.Common
+{
+    /// <summary>
+    /// Rules governing the meeting of Offer Voucher Threasholds
+    /// </summary>
+    internal class OfferVoucherThreasholdRule
+    {
+        //TODO move to database / config
+        private IEnumerable<ProductCategoryEnum> ExcludedProducts = new List<ProductCategoryEnum>()
+        {
+            ProductCategoryEnum.GiftVoucher
+        };
+
+        private ShoppingBasketDto Basket {get;set;}
+
+        /// <summary>
+        /// Determins f any products should not be evaluated in the conditon and executes the evaluation of basket toatl and offer threashold.
+        /// </summary>
+        internal ShoppingBasketDto Validate(ShoppingBasketDto basket)
+        {
+            Guardian.ArgumentNotNull(basket, "basket");
+
+            this.Basket = basket;
+
+            CheckBasketTotalMeetsThreashold();
+
+            return this.Basket;
+        }
+
+        private void CheckBasketTotalMeetsThreashold()
+        {
+            var tempBasket = CreateTempBasket();
+            tempBasket = RemoveExcludedProductsFrom(tempBasket);
+            tempBasket.CalculateTotal();
+
+            if (tempBasket.Total < Basket.OfferVoucher.Threashold)
+            {
+                Basket.Notifications.Add(
+                   new Notification(
+ new Error("You have not reached the spend threshold for voucher YYY-YYY. Spend another £{0} to receive £{1} discount from your basket total.".FormatLiteralArguments(0, 25))));
+            }
+        }
+
+        private ShoppingBasketDto CreateTempBasket()
+        {
+            var productList = new List<ProductDto>();
+            productList.AddRange(this.Basket.Products);
+            
+            var tempBasket = new ShoppingBasketDto(){
+                Products = productList
+            };
+
+            return tempBasket;
+        }
+
+        private ShoppingBasketDto RemoveExcludedProductsFrom(ShoppingBasketDto tempBasket)
+        {
+            this.Basket.Products.ForEach(p =>
+            {
+                if (ExcludedProducts.Contains(p.Category))
+                {
+                    tempBasket.Products.Remove(p);
+                }
+            });
+
+            return tempBasket;
+        }
+    }
+}
---
+++ WiggleTechTest/Wiggle.Domain/PurchaseRules/ProductOfferRules.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wiggle.Domain.Models.ShoppingBasket;
+
+namespace Wiggle.Domain.PurchaseRules
+{
+    public class ProductOfferRules
+    {
+        public ShoppingBasketDto ApplyOfferVoucher(ShoppingBasketDto basket)
+        {
+            
+
+            if (!basket.Notifications.HasErrors)
+            {
+                var total = basket.GrossTotal;
+
+                if (basket.OfferVoucher.IsNotNull())
+                {
+                    total -= basket.OfferVoucher.Value;
+                }
+
+                basket.SubTotal = total;
+            }
+
+            return basket;
+        }
+    }
+}
