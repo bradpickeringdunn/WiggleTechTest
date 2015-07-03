996e33268503ea98aa12f17f0681b094e5f4cfe7
H.B Pickering-Dunn <brad@pickering-dunn.com>
2015-07-03 06:46:14 +0200

    Removed WCF test client

--- WiggleTechTest/Wiggle.Service.Models.Test/Properties/AssemblyInfo.cs
+++ WiggleTechTest/Wiggle.Service.Models.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wiggle.Client.Intergration.Test")]
+[assembly: AssemblyTitle("Wiggle.Service.Models.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wiggle.Client.Intergration.Test")]
+[assembly: AssemblyProduct("Wiggle.Service.Models.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ce2127b7-634c-44dd-9ad4-e9bc7ea92192")]
+[assembly: Guid("fdbabdc4-5c9d-4a39-b05a-c3471a49b119")]
 
 // Version information for an assembly consists of the following four values:
 //
